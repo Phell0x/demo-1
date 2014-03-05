ac2a173e7ef0aaebda8018d9992afd2a64ca1149
lam <lam@cph-cnu1470h3l.efif.dk>
2014-03-05 11:05:12 +0100

    Intial Commit

---
+++ src/sletmig/SletMig.java
@@ -0,0 +1,16 @@
+package sletmig;
+
+/**
+ *
+ * @author Lars Mortensen (lam@cphbusiness.dk)
+ */
+public class SletMig {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
